80bb78d84d8f5219ad4b68112a1be33b377bca3d
srinivasan.sankaran <srinivasan.sankaran@BDC3-D-519QSWM.dir.svc.accenture.com>
2016-12-23 11:37:00 +0530

    Commit23_2

--- src/com/cloud/CloudCloud.java
+++ src/com/cloud/CloudCloud.java
@@ -2,18 +2,18 @@ package com.cloud;
 
 public class CloudCloud {
 	
-	private String name3;
+	private String name2;
 
 	
 	
-	private int id;
+	private int id1;
 
-	public int getId() {
-		return id;
+	public int getId1() {
+		return id1;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setId(int id1) {
+		this.id1 = id1;
 	}
 	
 
